b06262c9320f9549356738216e37abf101b68722
Abed-elfattah <abd_yacoub19@hotmail.com>
2023-12-28 15:25:28 +0200

    init

---
+++ .history/example/lib/config_20231227135136.dart
@@ -0,0 +1,9 @@
+// const Map configs = {
+//   'ZOOM_SDK_KEY': '',
+//   'ZOOM_SDK_SECRET': '',
+// };
+
+const Map configs = {
+  'ZOOM_SDK_KEY': 'ZwiqXTB0SNC97vFCwTjUJA',
+  'ZOOM_SDK_SECRET': 'FniM8sKuVxmhOch2cliFonpzQopsUp07ZVEz',
+};
