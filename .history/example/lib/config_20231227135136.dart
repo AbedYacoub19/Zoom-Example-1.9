161fcc345d0af0b308cbf8a94ca998f3f8edd8ec
AbedYacoub19 <38328806+AbedYacoub19@users.noreply.github.com>
2023-12-28 17:44:34 +0300

    Delete .history/example/lib/config_20231227135136.dart

--- .history/example/lib/config_20231227135136.dart
+++
@@ -1,9 +0,0 @@
-// const Map configs = {
-//   'ZOOM_SDK_KEY': '',
-//   'ZOOM_SDK_SECRET': '',
-// };
-
-const Map configs = {
-  'ZOOM_SDK_KEY': 'ZwiqXTB0SNC97vFCwTjUJA',
-  'ZOOM_SDK_SECRET': 'FniM8sKuVxmhOch2cliFonpzQopsUp07ZVEz',
-};
